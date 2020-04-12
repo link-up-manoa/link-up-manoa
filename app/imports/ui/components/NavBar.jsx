c68f50b15942227bc206560d77e573a7fa7c9bc5
Taylor D. Gabatino <tgabatin@hawaii.edu>
2020-04-12 05:46:00 -1000

    implemented study session page, fixed navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -18,10 +18,12 @@ class NavBar extends React.Component {
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <Header inverted as='h1'>Link Up Manoa</Header>
         </Menu.Item>
-        <Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>User Page</Menu.Item>,
-        <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Create Study Session</Menu.Item>,
-        <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>User Search</Menu.Item>,
-        <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Friends Page</Menu.Item>
+        {this.props.currentUser ? (
+            [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>User Page</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/create" key='list'>Create Study Session</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>User Search</Menu.Item>,
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>Friends Page</Menu.Item>]
+        ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
             <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
         ) : ''}
---
+++ app/imports/ui/pages/CreateStudySession.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
+import { AutoForm, ErrorsField, NumField, SelectField, SubmitField, TextField } from 'uniforms-semantic';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import SimpleSchema from 'simpl-schema';
+import { Stuffs } from '../../api/stuff/Stuff';
+
+/** Create a schema to specify the structure of the data to appear in the form. */
+const formSchema = new SimpleSchema({
+  username: String,
+  time: Number,
+  date: Date,
+  place: String,
+  members: String,
+  topic: String,
+  questions: String,
+});
+
+/** Renders the Page for adding a document. */
+class CreateStudySession extends React.Component {
+
+  /** On submit, insert the data. */
+  submit(data, formRef) {
+    const { username, time, date, place, members, topic, questions } = data;
+    const owner = Meteor.user().username;
+    Stuffs.insert({ username, time, date, place, members, topic, questions, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  render() {
+    let fRef = null;
+    return (
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center" inverted>Create Study Session</Header>
+            <AutoForm ref={ref => { fRef = ref; }} schema={formSchema} onSubmit={data => this.submit(data, fRef)} >
+              <Segment>
+                <TextField name='username'/>
+                <NumField name='time' decimal={false}/>
+                <NumField name='date' decimal={false}/>
+                <TextField name='place'/>
+                <TextField name='members'/>
+                <TextField name='topic'/>
+                <TextField name='questions'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
+    );
+  }
+}
+
+export default CreateStudySession;
