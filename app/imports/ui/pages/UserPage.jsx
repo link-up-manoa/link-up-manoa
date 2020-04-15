8fc04bd6e26be51bde594c9bfcb4f58f5bf5e567
Jatin Pandya <jatinp@hawaii.edu>
2020-04-15 11:17:53 -1000

    Complete Issue 2

--- app/imports/ui/pages/UserPage.jsx
+++ app/imports/ui/pages/UserPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 // import _ from 'underscore@1.0.10';
-import { Container, Header, Loader, Card, Image } from 'semantic-ui-react';
+import { Container, Header, Loader, Card, Image, Button, Icon, Grid } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Users } from '../../api/user/User';
@@ -29,8 +29,29 @@ class UserPage extends React.Component {
                 <Card.Meta>Status: {this.props.user[0].status}</Card.Meta>
                 <Card.Meta>Major: {this.props.user[0].dType} - {this.props.user[0].mType} </Card.Meta>
                 <Card.Description>
-                  Classes Taken: ICS 111
+                  Classes Taken: ICS 111, 141, 211, 212, 241, 311, 312, 314, 451
                 </Card.Description>
+                <Card.Description>
+                  Classes Currently In: ICS 426, 313, 332, 321
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <Grid columns='two' divided>
+                  <Grid.Row>
+                    <Grid.Column>
+                      <Button icon labelPosition='left'>
+                        <Icon name='add' />
+                        Add Class
+                      </Button>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Button icon labelPosition='left' >
+                        <Icon name='edit' />
+                        Edit User
+                      </Button>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
               </Card.Content>
             </Card>
           </Card.Group>
